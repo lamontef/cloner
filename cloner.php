527e0f118e724ceb589faed5c7dc0fe8ab6fa174
LaMonte M Forthun <lamontef@yahoo.com>
2022-01-26 01:54:51 -0500

    Update cloner.php

--- cloner.php
+++ cloner.php
@@ -31,6 +31,24 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+include_once('gitupdate.php');
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+			'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/hikikomorime/cloner', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/hikikomorime/cloner/master', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/hikikomorime/cloner', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/hikikomorime/cloner/zipball/master', // the zip url of the GitHub repo
+			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.0', // which version of WordPress does your plugin require?
+			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+			'readme' => 'README.md', // which file to use as the readme for the version number
+			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+		);
+		new WP_GitHub_Updater($config);
+	}
 
 class WPMUDEV_Cloner {
 
